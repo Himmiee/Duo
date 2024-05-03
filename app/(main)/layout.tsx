ba87336b1cb72fb9a4cef81c65ed7b73f95cbcc2
Haliyah Tijani <haliyya460@gmail.com>
2024-05-03 18:26:37 +0100

    learn page

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { Sidebar } from "@/components/sidebar";
+import { MobileHeader } from "@/components/mobile-header";
 
 type Props = {
     children: React.ReactNode;
@@ -9,6 +10,7 @@ const MainLayout = ({
 }: Props) => {
     return (
         <>
+        <MobileHeader/>
         <Sidebar className="hidden lg:flex"/>
            <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0"> <div className="bg-red-500 h-full">
            {children}</div></main>
