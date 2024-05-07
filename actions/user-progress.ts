5afdaf44355cd528aa87722c22d8de3f5be7d994
Haliyah Tijani <haliyya460@gmail.com>
2024-05-07 22:53:51 +0100

    courses update

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -1,35 +1,49 @@
-"use server"
+"use server";
 
 import db from "@/db/drizzle";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { getCourseById, getUserProgress } from "@/db/queries";
 import { userProgress } from "@/db/schema";
 import { auth, currentUser } from "@clerk/nextjs/server";
 
 export const upsertUserProgress = async (courseId: number) => {
-    const {userId} = await auth()
-    const user = await currentUser()
+  const { userId } = await auth();
+  const user = await currentUser();
 
-    if (!userId || !user){
-        throw new Error("Unauthorized")
-    }
+  if (!userId || !user) {
+    throw new Error("Unauthorized");
+  }
 
-    const course = await getCourseById(courseId)
+  const course = await getCourseById(courseId);
 
-    if (!course) {
-        throw new Error("Course not found")
-    }
+  if (!course) {
+    throw new Error("Course not found");
+  }
 
-    // Todo :enable units
-    // if (!course.units.length || !course.units[0].lessons.length) {
-    //     throw new Error("Course has no units or lessons")
-    // }
+  // Todo :enable units
+  // if (!course.units.length || !course.units[0].lessons.length) {
+  //     throw new Error("Course has no units or lessons")
+  // }
 
-    const existingUserProgress = await getUserProgress()
-    if (existingUserProgress){
-        await db.update(userProgress).set({
-            activeCourseId: courseId,
-            userName: user.firstName || "User",
-            userImageSrc: user.imageUrl || "/mainlogo.svg",
-        })
-    } 
-}
+  const existingUserProgress = await getUserProgress();
+  if (existingUserProgress) {
+    await db.update(userProgress).set({
+      activeCourseId: courseId,
+      userName: user.firstName || "User",
+      userImageSrc: user.imageUrl || "/mainlogo.svg",
+    });
+    revalidatePath("/courses");
+    revalidatePath("/learn");
+    redirect("/learn");
+  }
+  await db.insert(userProgress).values({
+    userId,
+    activeCourseId: courseId,
+    userName: user.firstName || "User",
+    userImageSrc: user.imageUrl || "/mainlogo.svg",
+  });
+  revalidatePath("/courses");
+  revalidatePath("/learn");
+  redirect("/learn");
+};
--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -4,6 +4,8 @@ import { courses, userProgress } from "@/db/schema";
 import { Card } from "./card";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
+import { upsertUserProgress } from "@/actions/user-progress";
+import { toast } from "sonner";
 
 type Props = {
   courses: (typeof courses.$inferSelect)[];
@@ -16,12 +18,12 @@ export const List = ({ courses, activeCourseId }: Props) => {
 
   const onClick = (id: number) => {
     if (pending) return;
-    if (id === activeCourseId){
-      return router.push("/learn")
+    if (id === activeCourseId) {
+      return router.push("/learn");
     }
-    startTransition(()=> {
-      
-    })
+    startTransition(() => {
+      upsertUserProgress(id).catch(() => toast.error("Something went wrong"));
+    });
   };
   return (
     <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
@@ -31,8 +33,8 @@ export const List = ({ courses, activeCourseId }: Props) => {
           id={course.id}
           title={course.title}
           imageSrc={course.imageSrc}
-          onClick={() => {}}
-          disabled={false}
+          onClick={onClick}
+          disabled={pending}
           active={course.id === activeCourseId}
         />
       ))}
