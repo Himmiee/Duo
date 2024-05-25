41ad0f8491589aa654c11805e83743de8e9c6069
Haliyah Tijani <haliyya460@gmail.com>
2024-05-25 02:03:32 +0100

    schema and drizzle orm upgrade changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
+import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
-import type { Config } from "drizzle-kit";
 
-export default {
-  schema: "./db/schema.ts",
-  out: "./drizzle",
-  driver: "pg",
+export default defineConfig({
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
-} satisfies Config;
+  migrations: {
+    table: "migrations",
+    schema: "public",
+  },
+  schema: "./db/schema.ts", // add this line to specify the schema file
+  out: "./drizzle", // directory for generated files
+});
